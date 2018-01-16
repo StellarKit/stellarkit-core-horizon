37b3c9a56534986b49c498b011f0256d764b2c76
Steve Gehrman <sgehrman@gmail.com>
2018-01-15 18:27:34 -0800

    sf

--- horizon-docker/install.sh
+++ horizon-docker/install.sh
@@ -9,12 +9,13 @@ then
     && curl https://glide.sh/get | sh \
     && glide install \
     && go install github.com/stellar/go/services/horizon
+
+  mv /go/bin/horizon /usr/local/bin
 else
   wget -O horizon.tar.gz https://github.com/stellar/horizon/releases/download/v${HORIZON_VERSION}/horizon-v${HORIZON_VERSION}-linux-amd64.tar.gz
   tar -zxvf horizon.tar.gz
   mv /horizon-v${HORIZON_VERSION}-linux-amd64/horizon /usr/local/bin
   chmod +x /usr/local/bin/horizon
-  rm -rf horizon.tar.gz /horizon-v${HORIZON_VERSION}-linux-amd64
 
   echo "\nDone installing stellar-core...\n"
 fi
