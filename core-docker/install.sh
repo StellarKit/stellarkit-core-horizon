c3aa06cb623e514afb17b5fd3952c2f493182146
Steve Gehrman <sgehrman@gmail.com>
2018-01-15 17:53:40 -0800

    sdf

---
+++ core-docker/install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $BUILD_FROM_SRC -gt 0 ]
+then
+  # deploy stellar-core binary
+  RUN mkdir -p /go/src/github.com/stellar/ \
+    && git clone --depth 1 --branch master https://github.com/stellar/stellar-core /stellar-core \
+    && cd /stellar-core \
+    && git submodule init \
+    && git submodule update \
+    && ./autogen.sh \
+    && ./configure \
+    && make -j \
+    && make install
+else
+  wget -O stellar-core.deb https://s3.amazonaws.com/stellar.org/releases/stellar-core/stellar-core-${STELLAR_CORE_VERSION}_amd64.deb
+  dpkg -i stellar-core.deb
+  rm stellar-core.deb
+
+  echo "\nDone installing stellar-core...\n"
+fi
---
+++ horizon-docker/install.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+if [ $BUILD_FROM_SRC -gt 0 ]
+then
+  # deploy horizon binary
+  RUN mkdir -p /go/src/github.com/stellar/ \
+    && git clone --depth 1 --branch master https://github.com/stellar/go.git /go/src/github.com/stellar/go \
+    && cd /go/src/github.com/stellar/go \
+    && curl https://glide.sh/get | sh \
+    && glide install \
+    && go install github.com/stellar/go/services/horizon
+else
+  wget -O horizon.tar.gz https://github.com/stellar/horizon/releases/download/v${HORIZON_VERSION}/horizon-v${HORIZON_VERSION}-linux-amd64.tar.gz
+  tar -zxvf horizon.tar.gz
+  mv /horizon-v${HORIZON_VERSION}-linux-amd64/horizon /usr/local/bin
+  chmod +x /usr/local/bin/horizon
+  rm -rf horizon.tar.gz /horizon-v${HORIZON_VERSION}-linux-amd64
+
+  echo "\nDone installing stellar-core...\n"
+fi
